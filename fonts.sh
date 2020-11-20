ef420f70aeb1af0e503005dfc651338649dad203
Eddy Luten <eddyluten@gmail.com>
2020-11-20 13:12:35 -0800

    VSCode changes, font added, and kb remapping removed

--- fonts.sh
+++ fonts.sh
@@ -9,4 +9,8 @@ git clone https://github.com/adobe-fonts/source-serif-pro.git
 cp ~/.fonts/source-serif-pro/TTF/*.ttf ~/.fonts
 rm -rf ~/.fonts/source-serif-pro
 
+# JetBrains Mono
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
+
 fc-cache -f -v
+
